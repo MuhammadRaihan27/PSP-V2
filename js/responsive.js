a507add1fbb73e5e66187ec3eab74eef7816ded2
MuhammadRaihan27 <r41h4n.mrn@gmail.com>
2022-01-05 12:54:34 +0700

    Update image

---
+++ js/responsive.js
@@ -0,0 +1,11 @@
+const IMG = document.querySelector(".navbar-brand img");
+console.log(IMG)
+const dataIMG1 = "/assets/img/logo.png";
+const dataIMG2 = "/assets/img/LOGO2.png";
+
+if (window.innerWidth < 500) {
+    IMG.setAttribute("src", `${dataIMG2}`)
+} else {
+    IMG.setAttribute("src", `${dataIMG1}`)
+
+}

248e2c852d20d0e55e0ab369634d0f6984e5fe9f
MuhammadRaihan27 <r41h4n.mrn@gmail.com>
2022-01-06 21:47:46 +0700

    update logo

--- js/responsive.js
+++ js/responsive.js
@@ -1,9 +1,9 @@
-// const IMG = document.querySelector(".navbar-brand img");
-// const dataIMG1 = "/assets/img/logo.png";
-// const dataIMG2 = "/assets/img/LOGO2.png";
+const IMG = document.querySelector(".navbar-brand img");
+const dataIMG1 = "/assets/img/Logo.png";
+const dataIMG2 = "/assets/img/LOGO2.png";
 
-// if (window.innerWidth < 500) {
-//     IMG.setAttribute("src", `${dataIMG2}`)
-// } else {
-//     IMG.setAttribute("src", `${dataIMG1}`)
-// }
+if (window.innerWidth < 500) {
+    IMG.setAttribute("src", `${dataIMG2}`)
+} else {
+    IMG.setAttribute("src", `${dataIMG1}`)
+}
